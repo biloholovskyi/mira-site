e19a9032757f14ac92ab2c725ffb9c6fbc4abbf8
Biloholovskyi <biloholovskyi>
2021-06-15 20:34:13 +0300

    Grid

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,17 @@
 import React from "react";
 
+import HomePage from "../../pages/homePage/homePage";
+
 import * as Style from './styled'
 
 const App = () => {
   return (
-    <Style.Wrapper>
-      Дарим финансовую независимость каждому
-    </Style.Wrapper>
+    <>
+      <Style.Wrapper>
+        Дарим финансовую независимость каждому
+      </Style.Wrapper>
+      <HomePage/>
+    </>
   )
 }
 
---
+++ src/pages/homePage/homePage.js
@@ -0,0 +1,25 @@
+import React from "react"
+import {Container, Row, Col} from "react-bootstrap";
+
+const HomePage = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default HomePage;
