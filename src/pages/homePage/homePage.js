14dc4ac370b9be0c9ef6e95dd0efa2a574ce8def
Biloholovskyi <biloholovskyi>
2021-06-16 18:27:05 +0300

    mira deposit

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,15 +1,66 @@
 import React from "react"
+import {Container, Row, Col} from "react-bootstrap";
 
 import MainBlock from "./mainBlock/mainBlock";
 import MainDesc from "./mainDesc/mainDesc";
+import DefaultTitle from "../../components/defaultTitle/defaultTitle";
+import MainButton from "../../components/buttons/mainButton/mainButton";
 
 import * as Style from './styled'
+
+import imgDeposit from './media/image.png'
 
 const HomePage = () => {
   return (
     <Style.Wrapper>
       <MainBlock/>
       <MainDesc/>
+
+      {/* mira deposit */}
+      <Style.TextAndImage>
+        <Container>
+          <Row>
+            <Col lg={6}>
+              <div className="text">
+                <DefaultTitle
+                  text={'Mira deposit'}
+                  style={{
+                    marginBottom: '40px'
+                  }}
+                />
+
+                <p className="desc">Инвестирование в базовый индекс MIRA Index позволяет вам надежно и и с высоким
+                  уровнем дохода разместить средства в лучшие финансовые активы десятилетия:</p>
+
+                <div className="item">Криптовалюты (трейдинг внутри дня, фарминг, стейкинг)</div>
+                <div className="item">Фондовый рынок (акции крупных производственных компаний, IT-стартапов)</div>
+                <div className="item">Инвестиции в IT-стартапы на ранней стадии перед выходом на Pre-IPO/IPO</div>
+                <div className="item">Торги на рынке FOREX</div>
+                <div className="item">Заработок на программах обучения Mira University</div>
+
+                <div className="info">
+                  <div className="info-item">
+                    <div className="name">Сумма депозита</div>
+                    <div className="value">от $100</div>
+                  </div>
+
+                  <div className="info-item">
+                    <div className="name">Ставка</div>
+                    <div className="value">до 231%</div>
+                  </div>
+
+                  <MainButton text={'Оформить депозит'} type={'lg'}/>
+                </div>
+              </div>
+            </Col>
+            <Col lg={6}>
+              {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
+              <img src={imgDeposit} alt="image" className="image image-first"/>
+            </Col>
+          </Row>
+        </Container>
+      </Style.TextAndImage>
+      {/* /mira deposit */}
     </Style.Wrapper>
   )
 }
--- src/pages/homePage/styled.js
+++ src/pages/homePage/styled.js
@@ -4,6 +4,91 @@ const Wrapper = styled.div`
   background-color: #121212;
 `
 
+const TextAndImage = styled.div`
+  overflow: hidden;
+  
+  .text {
+    padding: 120px 0;
+    
+    .desc {
+      font-family: TT Interfaces, sans-serif;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 18px;
+      line-height: 26px;
+      letter-spacing: 0.005em;
+      color: #FFFFFF;
+      margin-bottom: 20px;
+    }
+    
+    .item {
+      font-family: TT Interfaces, sans-serif;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 18px;
+      line-height: 26px;
+      letter-spacing: 0.005em;
+      color: #FFFFFF;
+      padding-left: 20px;
+      position: relative;
+      margin-bottom: 20px;
+      
+      &::before {
+        content: '';
+        position: absolute;
+        width: 8px;
+        height: 8px;
+        background-color: #36B37E;
+        left: 0;
+        top: 8px;
+        border-radius: 50%;
+      }
+    }
+
+    .info {
+      border-top: 1px solid rgba(255, 255, 255, 0.1);
+      margin-top: 40px;
+      padding-top: 40px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      
+      .info-item {
+        .name {
+          font-family: TT Interfaces, sans-serif;
+          font-style: normal;
+          font-weight: normal;
+          font-size: 16px;
+          line-height: 24px;
+          letter-spacing: 0.005em;
+          color: rgba(255, 255, 255, 0.64);
+        }
+        
+        .value {
+          font-family: TT Interfaces, sans-serif;
+          font-style: normal;
+          font-weight: normal;
+          font-size: 24px;
+          line-height: 32px;
+          letter-spacing: 0.01em;
+          color: #FFFFFF;
+        }
+      }
+    }
+  }
+  
+  .image {
+    height: 100%;
+    object-position: center;
+    object-fit: contain;
+    
+    &-first {
+      margin-top: -60px;
+    }
+  }
+`
+
 export {
-  Wrapper
+  Wrapper,
+  TextAndImage
 }
